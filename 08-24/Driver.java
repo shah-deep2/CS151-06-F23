d35d2175e329b08f93a5ce1acb9b17ff39c76755
shah-deep2 <126208047+shah-deep2@users.noreply.github.com>
2023-08-24 16:00:37 -0700

    Add files via upload

---
+++ 08-24/Driver.java
@@ -0,0 +1,79 @@
+import shapes.*;
+
+class Driver {
+    // double area(int radius) {
+    //     return Math.PI * radius * radius;
+    // }
+    public static void main(String[] args) {
+        // int n;
+
+        // boolean tf;
+
+        Circle c1 = new Circle();
+
+        // Circle c2;
+
+        // int radius = 5;
+
+        // double area = Math.PI * radius * radius * 2;
+
+        // System.out.println(area);
+
+        // radius = 7;
+
+        // area = Math.PI * radius * radius * 2;
+
+        // System.out.println(area);
+
+        // Circle c1 = new Circle();
+        
+        // System.out.println(c1.area());
+
+        // c1.radius = 2;
+        // System.out.println(c1.area());
+
+        // c1.radius = 7;
+        // System.out.println(c1.area());
+
+        // Circle c2 = new Circle();
+
+
+        // Create a class Rectangle
+        // Create its attributes
+        // Create methods to find Area & Perimeter for this rectangle
+        // Instantiate an object of the Rectangle in the Driver
+        // Assign values to its attributes
+        // Print area & perimeter
+
+        // Rectangle r1 = new Rectangle();
+
+        // r1.breath = 2;
+        // r1.length = 4;
+
+        // System.out.println(r1.area());
+        // System.out.println(r1.perimeter());
+
+        Rectangle r2 = new Rectangle();
+
+        // r2.breath = 2;
+        // r2.length = 5;
+
+        // System.out.println(r1 == r2);
+
+        // Rectangle r3;
+        // r3 = r1;
+
+        // System.out.println(r1 == r3);
+
+
+        // Create a file Triangle in shapes 
+        // public class Triangle {}
+        // import it in Driver
+        // Create variable Triangle t;
+
+        Triangle t;
+        
+
+
+    }
+}
---
+++ 08-24/Test.java
@@ -0,0 +1,15 @@
+import shapes.*;
+class TestPrinter {
+    public static void main(String[] args) {
+        System.out.println("Test Printer");
+        new Rectangle().type();
+    }
+}
+
+
+class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test2");
+        Circle c;
+    }
+}
