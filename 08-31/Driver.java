5c1413b91233d4b5dfd3b04c76bc990fb04a79d3
Deep Shah <deepshah11111@gmail.com>
2023-09-12 13:33:15 -0700

    08-31

---
+++ 08-31/Driver.java
@@ -0,0 +1,42 @@
+public class Driver {
+
+    /*
+        Line 1
+        Line 2
+     */
+    //  Line
+    // Line 2
+
+    /**
+     * 
+     * @return Returns an integer 1
+     */
+
+    public int getInt(){
+        return 1;
+    }
+
+
+    /**
+     * 
+     * @param args any strings
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello");        // Print Hello
+    }
+
+
+    /**
+     * Takes a string and returns it.
+     * 
+     * @param givenName Takes a name.
+     * @return Returns the name.
+     */
+
+    public String getName(String givenName){
+        return givenName;
+    }
+}
+
+// End
