c07563cf4e3fc11bfa129b7542b6ea5ed210c203
Deep Shah <deepshah11111@gmail.com>
2023-10-10 18:43:05 -0700

    Updated Class Diagram

--- 10-05 & 10-10/Department.java
+++ 10-05 & 10-10/Department.java
@@ -3,5 +3,4 @@ import java.util.List;
 class Department {
 	private List<Student> students;  // Department has Students but both can independently exist
 	private List<Course> courses;    // Department has courses; Course cannot exist without department
-	
 }
